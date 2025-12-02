ba129477f610984608525714449d874a7a55daec
alouguim <alexandredouglas16@gmail.com>
2025-12-02 11:10:08 -0300

    mais interacoes

--- Objetos/LocalChatissimo.java
+++ Objetos/LocalChatissimo.java
@@ -13,11 +13,13 @@ public class LocalChatissimo extends Local {
     @Override
     public void interagirSozinho(Personagem p) {
         String[] interacoes = {
-            p.getNome() + " interaçao1.",
-            p.getNome() + " interacao2.",
-            p.getNome() + " interacao3.",
-            p.getNome() + " interacao4.",
-            p.getNome() + " interacao5."
+            p.getNome() + " olha as horas no relógio da" + this.getNome() + "e percebe que o tempo está correndo ao contrário.",
+            p.getNome() + " bate a própria cabeça na parede repeditamente para sentir algo.",
+            p.getNome() + " encara o abismo e torce para que ele o encare de volta.",
+            p.getNome() + " resolve estudar para passar o tempo.",
+            p.getNome() + " planeja uma rotina para mudar de vida.",
+            p.getNome() + " suspira profundamente, como se esperasse que o ar respondesse.",
+            p.getNome() + " procura algo interessante no chão da " + this.getNome() + ", mas não encontra nem poeira."
         };
         System.out.println(interacoes[r.nextInt(interacoes.length)]);
     }
@@ -25,11 +27,13 @@ public class LocalChatissimo extends Local {
     @Override
     public void interagir2Personagens(Personagem p1, Personagem p2) {
         String[] interacoes = {
-            p1.getNome() + " interacao1 " + p2.getNome() + " interacao1.",
-            p1.getNome() + " e " + p2.getNome() + " interacao2.",
-            p1.getNome() + " interacao3 " + p2.getNome() + " interacao3",
-            p1.getNome() + " e " + p2.getNome() + " interacao4.",
-            p1.getNome() + " interacao5 " + p2.getNome() + ", interacao5."
+            p1.getNome() + " verifica se " + p2.getNome() + " ainda está vivo e percebe que sim. Entretanto, se está acordado já é outra história.",
+            p1.getNome() + " e " + p2.getNome() + " fazem doomscrolling em seus celulares ao mesmo tempo.",
+            p1.getNome() + " tenta chamar " + p2.getNome() + " para fazer alguma coisa, mas nada vem em mente.",
+            p1.getNome() + " e " + p2.getNome() + " estão paralisados e não se movem a 40 minutos.",
+            p1.getNome() + " busca animar as coisas com " + p2.getNome() + ", mas ficam entediados assim que entram na"+ this.getNome() + " .",
+            p1.getNome() + " sugere que " + p2.getNome() + " toque no botão misterioso da " + this.getNome() + ". Ambos se arrependem imediatamente.",
+            p1.getNome() + " e " + p2.getNome() + " ficam encarando o teto da " + this.getNome() + " como se esperassem uma revelação divina."
         };
         System.out.println(interacoes[r.nextInt(interacoes.length)]);
     }
@@ -37,11 +41,15 @@ public class LocalChatissimo extends Local {
     @Override
     public void interagir3Personagens(Personagem p1, Personagem p2, Personagem p3) {
         String[] interacoes = {
-            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " interacao1.",
-            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " interacao2.",
-            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " interacao3.",
-            p1.getNome() + " interacao4 " + p2.getNome() + " e " + p3.getNome() + ", mas só sai ruído indecifrável.",
-            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " interacao5."
+             p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " entram na " + this.getNome() + " e imediatamente esquecem por que vieram.",
+            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " tentam conversar, mas cada um começa um assunto diferente ao mesmo tempo.",
+            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " ficam olhando para um ponto aleatório da " + this.getNome() + " sem motivo aparente.",
+            p1.getNome() + " tenta falar algo com " + p2.getNome() + " e " + p3.getNome() + " na " + this.getNome() + ", mas só sai um ruído indecifrável.",
+            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " decidem se motivar mutuamente, falham e acabam deitados no chão.",
+            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " fazem um pacto silencioso de não fazer absolutamente nada.",
+            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " percebem que ninguém sabe como sair da " + this.getNome() + ".",
+            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " abrem uma discussão filosófica que termina em risadas desconfortáveis.",
+            p2.getNome() + " e " + p3.getNome() + " encontram um papel escrito 'Só trabalho sem diversão faz de " + p1.getNome() + " um bobão.\nSó trabalho sem diversão faz de " + p1.getNome() + "um bobão.\nSó trabalho sem diversão faz de " + p1.getNome() + " um bobão...' e resolvem sair do local"
         };
         System.out.println(interacoes[r.nextInt(interacoes.length)]);
     }
--- Objetos/LocalRomantico.java
+++ Objetos/LocalRomantico.java
@@ -13,11 +13,11 @@ public class LocalRomantico extends Local {
     @Override
     public void interagirSozinho(Personagem p) {
         String[] interacoes = {
-            p.getNome() + " interaçao1.",
-            p.getNome() + " interacao2.",
-            p.getNome() + " interacao3.",
-            p.getNome() + " interacao4.",
-            p.getNome() + " interacao5."
+            p.getNome() + " se pergunta se vai ser amado de verdade um dia.",
+            p.getNome() + " percebe que está muito sozinho para estar na ." + this.getNome() + " e vai embora,",
+            p.getNome() + " olha para cima e sente falta de seu grande amor.",
+            p.getNome() + " observa os outros casais na " + this.getNome() + " com nojo.",
+            p.getNome() + " fecha os olhos e deixa a atmosfera romântica de " + this.getNome() + " envolver seus pensamentos."
         };
         System.out.println(interacoes[r.nextInt(interacoes.length)]);
     }
@@ -25,11 +25,17 @@ public class LocalRomantico extends Local {
     @Override
     public void interagir2Personagens(Personagem p1, Personagem p2) {
         String[] interacoes = {
-            p1.getNome() + " interacao1 " + p2.getNome() + " interacao1.",
-            p1.getNome() + " e " + p2.getNome() + " interacao2.",
-            p1.getNome() + " interacao3 " + p2.getNome() + " interacao3",
-            p1.getNome() + " e " + p2.getNome() + " interacao4.",
-            p1.getNome() + " interacao5 " + p2.getNome() + ", interacao5."
+            p1.getNome() + " e " + p2.getNome() + " trocam olhares tímidos enquanto o clima de " + this.getNome() + " fica mais intenso.",
+            p1.getNome() + " tenta iniciar uma conversa com " + p2.getNome() + ", mas ambos ficam corados e desviam o olhar.",
+            p1.getNome() + " e " + p2.getNome() + " caminham lado a lado, sentindo uma conexão silenciosa.",
+            p1.getNome() + " nota o sorriso de " + p2.getNome() + " e sente o coração acelerar levemente.",
+            p1.getNome() + " e " + p2.getNome() + " compartilham um momento leve e confortável, como se o tempo tivesse abrandado.",
+            p1.getNome() + " passa por debaixo do visgo. " + p2.getNome() + " ri desconfortavelmente e pergunta 'como isso foi parar aí?' enquanto esconde um pacote de sementes atrás das costas",
+            p1.getNome() + " decide que vai falar que queria ter beijado " + p2.getNome() + ". Mas só depois que eles voltarem pra casa.",
+            p1.getNome() + " e " + p2.getNome() + " se aninham como passarinhos.",
+            p1.getNome() + " esbarra de leve em " + p2.getNome() + " e ambos sorriem.",
+            p1.getNome() + " olha nos olhos de " + p2.getNome() + " por alguns segundos, sem dizer nada.",
+            p1.getNome() + " fica triste pensando em " + p2.getNome()
         };
         System.out.println(interacoes[r.nextInt(interacoes.length)]);
     }
@@ -37,11 +43,15 @@ public class LocalRomantico extends Local {
     @Override
     public void interagir3Personagens(Personagem p1, Personagem p2, Personagem p3) {
         String[] interacoes = {
-            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " interacao1.",
-            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " interacao2.",
-            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " interacao3.",
-            p1.getNome() + " interacao4 " + p2.getNome() + " e " + p3.getNome() + ", mas só sai ruído indecifrável.",
-            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " interacao5."
+             p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " conversam calmamente, aproveitando a atmosfera suave de " + this.getNome() + ".",
+            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " riem juntos, criando lembranças aconchegantes.",
+            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " sentem que o ambiente romântico parece aproximá-los mais.",
+            p1.getNome() + " nota a cumplicidade entre " + p2.getNome() + " e " + p3.getNome() + ", resultando num sorriso discreto.",
+            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " trocam histórias sob o clima acolhedor de " + this.getNome() + ".",
+            p1.getNome() + " pergunta se " + p2.getNome() + " e " + p3.getNome() + " estão ficando. Eles negam, mas " + p1.getNome() + " mostra um print em que ambos logaram Paddington 2 ao mesmo tempo no Letterboxd.",
+            p1.getNome() + " faz uma piada que deixa " + p2.getNome() + " rindo descontrolavelmente. " + p3.getNome() + " faz cara feia pra ele.",
+            p1.getNome() + ", " + p2.getNome() + " e " + p3.getNome() + " fazem um pacto de se casarem entre eles se não conseguirem arranjar ninguém em 30 anos.",
+            p1.getNome() + " observa discretamente " + p2.getNome() + " e " + p3.getNome() + " caminhando de mãos dadas."
         };
         System.out.println(interacoes[r.nextInt(interacoes.length)]);
     }
